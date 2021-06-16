5672630e718a58af022a380a67f0bf498052e74f
CosmicAnon <cosmicanon1320@gmail.com>
2021-06-16 13:00:27 +0530

    updates

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./style.scss";
+import { ghoulLogo, ghostdaiLogo } from "../../images";
 
 export const Footer = () => (
   <footer>
     <div className="container">
       <div className="copyright">
         <p>2021 &copy; Launch<b>BOX</b></p>
+      <div>
+        <p>
+          <a href="https://ghoultoken.io"><img className="ghoulIcons" alt="GHOUL" src={ghoulLogo}/></a>
+          <a href="https://ghoultoken.io"><img className="ghoulIcons" alt="GHOSTDAI" src={ghostdaiLogo}/></a>
+          <a href="https://www.ghostbyjohnmcafee.com"><img className="ghostIcon" alt="GHOST" src="https://www.ghostbyjohnmcafee.com/static/media/joincommunitylogo.3f526663.svg"/></a>
+        </p>
+
+      </div> 
         <a href="#top">Back to Top</a>
       </div>
     </div>
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./style.scss";
 import { Lottie } from "..";
-import { ghoulLogo, ghostdaiLogo } from "../../images";
 import floating from "./floating_anim.json";
 
 
@@ -15,21 +14,7 @@ export const Landing = () => (
       <a href="/"><span className="launchapp"></span>Launch App <span style={{ fontSize: 10, marginLeft: 5 }}>coming soon</span></a>
       <a href="https://t.me/poltergeistexchange" target="_blank"  rel="noreferrer" >
         <span className="telegramIcon"></span> Join Telegram
-      </a><br/><br/><br/>
-      <div>
-        <p>
-          Accepted Coins:<br/><br/>
-          <img className="ghoulIcons" alt="GHOUL" src={ghoulLogo}/>
-          <img className="ghoulIcons" alt="GHOSTDAI" src={ghostdaiLogo}/>
-          <img className="ghostIcon" alt="GHOST" src="https://www.ghostbyjohnmcafee.com/static/media/joincommunitylogo.3f526663.svg"/>
-          <span className="btcIcon"></span>
-          <span className="bchIcon"></span>
-          <span className="dashIcon"></span>
-          <span className="xmrIcon"></span>
-          <span className="zecIcon"></span>
-        </p>
-
-      </div>      
+      </a><br/><br/><br/>     
     </div>
     <div className="right">
       
