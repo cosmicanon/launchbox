b61f5f0e56ef9b582978ec1053fab352c5f1a4c6
CosmicAnon <cosmicanon1320@gmail.com>
2021-06-16 13:15:55 +0530

    updates

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,13 +6,12 @@ export const Footer = () => (
   <footer>
     <div className="container">
       <div className="copyright">
-        <p>2021 &copy; Launch<b>BOX</b><br/><br/>
+        <p>2021 &copy; Launch<b>BOX</b><br/>
           <a href="https://ghoultoken.io" target="_blank"  rel="noreferrer"><img className="ghoulIcons" alt="GHOUL" src={ghoulLogo}/></a>
           <a href="https://ghoultoken.io" target="_blank"  rel="noreferrer"><img className="ghoulIcons" alt="GHOSTDAI" src={ghostdaiLogo}/></a>
-          <a href="https://ghostbyjohnmcafee.com" target="_blank"  rel="noreferrer"><img className="ghostIcon" alt="GHOST" src="https://www.ghostbyjohnmcafee.com/static/media/joincommunitylogo.3f526663.svg"/></a>
+          <a href="https://www.ghostbyjohnmcafee.com" target="_blank"  rel="noreferrer"><img className="ghostIcon" alt="GHOST" src="https://www.ghostbyjohnmcafee.com/static/media/joincommunitylogo.3f526663.svg"/></a>
         </p>
-
-      </div> 
+ 
         <a href="#top">Back to Top</a>
       </div>
     </div>
